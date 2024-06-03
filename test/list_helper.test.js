0475f2a26b955316bb2ccfdae1d1c99584d1f708
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-06-03 12:50:57 +0200

    optimization avoid duplicated variables

--- test/list_helper.test.js
+++ test/list_helper.test.js
@@ -2,54 +2,53 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+    __v: 0,
+  },
+];
+
+const listWithMultipleBlogs = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f9",
+    title: "Another Blog Post",
+    author: "Another Author",
+    url: "https://example.com",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17fa",
+    title: "Yet Another Blog Post",
+    author: "Yet Another Author",
+    url: "https://example.com",
+    likes: 12,
+    __v: 0,
+  },
+];
+
+const listWithNoBlogs = [];
+
 test("dummy returns one", () => {
   const blogs = [];
-
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
 
 describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithMultipleBlogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f9",
-      title: "Another Blog Post",
-      author: "Another Author",
-      url: "https://example.com",
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17fa",
-      title: "Yet Another Blog Post",
-      author: "Yet Another Author",
-      url: "https://example.com",
-      likes: 12,
-      __v: 0,
-    },
-  ];
-
-  const listWithNoBlogs = [];
-
   test("when list has only one blog, equals the likes of that", () => {
     const result = listHelper.totalLikes(listWithOneBlog);
     assert.strictEqual(result, 5);
@@ -67,44 +66,6 @@ describe("total likes", () => {
 });
 
 describe("favorite blog", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithMultipleBlogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f9",
-      title: "Another Blog Post",
-      author: "Another Author",
-      url: "https://example.com",
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17fa",
-      title: "Yet Another Blog Post",
-      author: "Yet Another Author",
-      url: "https://example.com",
-      likes: 12,
-      __v: 0,
-    },
-  ];
-
   test("when list has only one blog, equals the likes of that blog", () => {
     const result = listHelper.favoriteBlog(listWithOneBlog);
     assert.deepStrictEqual(result, {
