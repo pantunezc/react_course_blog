0971295bf9d26c9437fc0091d01e5242324901c0
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-06-10 15:53:31 +0200

    exercice 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,19 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   try {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
     response.json(blogs);
   } catch (error) {
     response.status(500).send({ error: "Something went wrong" });
@@ -12,20 +22,30 @@ blogsRouter.get("/", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
   const { title, author, url, likes } = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
 
   if (!title || !url) {
     return response.status(400).json({ error: "title or url missing" });
   }
+
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title,
     author,
     url,
     likes: likes === undefined ? 0 : likes,
+    user: user._id,
   });
 
   try {
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.status(201).json(savedBlog);
   } catch (error) {
     response.status(500).send({ error: "Something went wrong" });
