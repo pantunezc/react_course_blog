b34347f8d8d1d9d166dde0cb01067452b54441b4
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-06-10 11:13:21 +0200

    exercice 4.15

---
+++ models/user.js
@@ -0,0 +1,29 @@
+// models/user.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  name: String,
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash; // the passwordHash should not be revealed
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
